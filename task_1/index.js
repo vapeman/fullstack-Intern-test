135d45d5bb0db432fd2857aa9e8a2e19078e998c
vapeman <kiv@vivaldi.net>
2024-05-14 19:18:44 +0300

    Fix output result

--- task_1/index.js
+++ task_1/index.js
@@ -6,3 +6,4 @@ import {EXCEPTIONS, SUFFIX} from "./consts.js";
 console.log('Wake up, Neo!')
 // console.log(encoded, translations);
 const decoded = decode(encoded, translations, EXCEPTIONS, SUFFIX);
+console.log(decoded)
